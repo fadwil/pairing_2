07841416b85b0f8ce988f91d32bb2f0cd295ab01
Wil Fady <wrfady@gmail.com>
2023-03-29 21:27:48 -0400

    Add or statement

--- plants.rb
+++ plants.rb
@@ -1,7 +1,9 @@
 gets_sunshine = true
-ounces_of_water = 10
+ounces_of_water = 9
 if gets_sunshine == true && ounces_of_water >= 10
     puts "Plant thrives"
+elsif gets_sunshine == true || ounces_of_water >= 10
+    puts "Plant meh"
 else 
     puts "Plant dead :/"
 end
